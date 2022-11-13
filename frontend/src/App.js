1821d86689ef441a44f5c15ccbe7b2d09788648e
anil babu <anilb9850@gmail.com>
2022-11-13 14:04:48 +0530

    done navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/header/Navbar";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Addproduct from "./components/product/Addproduct";
 function App() {
   return (
     <>
-      <h1>welcome to coding test</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/addproduct" element={<Addproduct />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/header/Navbar.jsx
+++ frontend/src/components/header/Navbar.jsx
@@ -1,7 +1,81 @@
-import React from "react";
-
+import React, { useState } from "react";
+import style from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isMobile, setisMobile] = useState(false);
+
+  return (
+    <>
+      <nav className={style.navbar}>
+        <h1>CodingTest</h1>
+
+        <ul className={isMobile ? style.mobilelinks : style.navlinks}>
+          <li>
+            <NavLink
+              className={style.cartlis}
+              onClick={() => setisMobile(false)}
+              to="/cart"
+              state={{ textDecoration: "none" }}
+            >
+              <span id="cart">
+                <ShoppingCartIcon />
+              </span>
+              <span id="cart_count">2</span>
+            </NavLink>
+          </li>
+          <li onClick={() => setisMobile(false)}>
+            <NavLink
+              to="/addproduct"
+              className={({ isActive }) =>
+                isActive ? style.active : style.home
+              }
+            >
+              Addproduct
+            </NavLink>
+          </li>
+          <li onClick={() => setisMobile(false)}>
+            <NavLink
+              to="/register"
+              className={({ isActive }) =>
+                isActive ? style.active : style.home
+              }
+            >
+              Register
+            </NavLink>
+          </li>
+          <li onClick={() => setisMobile(false)}>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? style.active : style.home
+              }
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+
+        <i
+          style={{ marginRight: "20px" }}
+          onClick={() => setisMobile(!isMobile)}
+          className={style.mobileMenuIcon}
+        >
+          {isMobile ? (
+            <>
+              <CloseIcon style={{ height: "40px" }} className={style.burger} />
+            </>
+          ) : (
+            <>
+              <MenuIcon style={{ height: "40px" }} className={style.burger} />
+            </>
+          )}
+        </i>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
